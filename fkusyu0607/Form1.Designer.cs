569602ecc7b76c4c26123aad0ddb856fd1ba274d
鎌田　拓臣 <yuuteayuu335dat@gmail.com>
2019-06-07 15:13:33 +0900

    up

--- fkusyu0607/Form1.Designer.cs
+++ fkusyu0607/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +63,33 @@
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -81,6 +105,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- fkusyu0607/Form1.cs
+++ fkusyu0607/Form1.cs
@@ -12,15 +12,30 @@ namespace fkusyu0607
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        private static Random rand = new Random();
 
-        Point cpos = MousePosition;
-        
+        int vx = rand.Next(-10,11);
+        int vy = rand.Next(-10, 11);
+
+        int vx2 = rand.Next(-10, 11);
+        int vy2 = rand.Next(-10, 11);
+
+        int vx3 = rand.Next(-10, 11);
+        int vy3= rand.Next(-10, 11);
+        int taim = 1000;
 
         public Form1()
         {
             InitializeComponent();
+
+            label1.Left = rand.Next(ClientSize.Width - label1.Width);
+            label1.Top = rand.Next(ClientSize.Width - label1.Height);
+
+            label2.Left = rand.Next(ClientSize.Width - label1.Width);
+            label2.Top = rand.Next(ClientSize.Width - label1.Height);
+
+            label2.Left = rand.Next(ClientSize.Width - label1.Width);
+            label2.Top = rand.Next(ClientSize.Width - label1.Height);
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -30,21 +45,45 @@ namespace fkusyu0607
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            cpos = PointToClient(cpos);
+            Point p = PointToClient(MousePosition);
 
-            textBox1.Left = cpos.X; //+ textBox1.Right / 2;
-            textBox1.Top = cpos.Y; //+ textBox1.Bottom / 2;
+            textBox1.Left = p.X;
+            textBox1.Top = p.Y ;
+
+            if ((p.X > label1.Left) && (p.X <= label1.Right) && (p.Y > label1.Top) && (p.Y < label1.Bottom) )
+                
+            {
+                vx = 0;
+                vy= 0;
+            }
+
+
+            if ((p.X > label2.Left) && (p.X <= label2.Right) && (p.Y > label2.Top) && (p.Y < label2.Bottom))
+            {
+                vx2 = 0;
+                vy2 = 0;
+            }
+
+            if((p.X > label3.Left) && (p.X <= label3.Right) && (p.Y > label3.Top) && (p.Y < label3.Bottom))
+            {
+                vx3 = 0;
+                vy3 = 0;
+            }
 
             label1.Left += vx;
             label1.Top += vy;
+            label2.Left += vx2;
+            label2.Top += vy2;
+            label3.Left += vx3;
+            label3.Top += vy3;
 
-            if (label1.Left >= 255)
+            if (label1.Right >= ClientSize.Width) 
             {
-                vx = -vx;
+                vx = -Math.Abs(vx);
             }
-            if (label1.Top >= 255)
+            if (label1.Bottom >= ClientSize.Width)
             {
-                vy =-vy;
+                vy = -Math.Abs(vy);
             }
             if (label1.Left <= 0)
             {
@@ -54,6 +93,45 @@ namespace fkusyu0607
             {
                 vy = Math.Abs(vy);
             }
+
+
+             if(label2.Right >= ClientSize.Width) 
+            {
+                vx2 = -Math.Abs(vx2);
+            }
+            if (label2.Bottom >= ClientSize.Width)
+            {
+                vy2 = -Math.Abs(vy2);
+            }
+            if (label2.Left <= 0)
+            {
+                vx2 = Math.Abs(vx2);
+            }
+            if (label2.Top <= 0)
+            {
+                vy2 = Math.Abs(vy2);
+            }
+
+
+             if  (label3.Right >= ClientSize.Width)
+            {
+                vx3 = -Math.Abs(vx3);
+            }
+            if (label3.Bottom >= ClientSize.Width)
+            {
+                vy3 = -Math.Abs(vy3);
+            }
+            if (label3.Left <= 0)
+            {
+                vx3 = Math.Abs(vx3);
+            }
+            if (label3.Top <= 0)
+            {
+                vy3 = Math.Abs(vy3);
+            }
+
+           taim--;
+         
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -61,6 +139,21 @@ namespace fkusyu0607
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
